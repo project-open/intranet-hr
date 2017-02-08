9c725c82616dd5688fb20b2e2ef2e74ae71e42cd
Frank Bergmann <frank.bergmann@project-open.com>
2017-02-08 09:48:43 +0000

    - Fixed creation scripts for OpenACS Installer

--- sql/postgresql/intranet-hr-create.sql
+++ sql/postgresql/intranet-hr-create.sql
@@ -164,6 +164,122 @@ create index im_employees_idx1 on im_employees(employee_id, supervisor_id);
 create index im_employees_idx2 on im_employees(supervisor_id, employee_id);
 
 
+
+
+-----------------------------------------------------------
+-- Full Text Search Engine
+--
+
+insert into im_search_object_types values (1,'user',5);
+
+create or replace function persons_tsearch () 
+returns trigger as $$
+declare
+	v_string	varchar;
+begin
+	select	coalesce(pa.email, '') || ' ' ||
+		coalesce(pa.url, '') || ' ' ||
+		coalesce(pe.first_names, '') || ' ' ||
+		coalesce(pe.last_name, '') || ' ' ||
+		coalesce(u.username, '') || ' ' ||
+		coalesce(u.screen_name, '') || ' ' ||
+
+		coalesce(home_phone, '') || ' ' ||
+		coalesce(work_phone, '') || ' ' ||
+		coalesce(cell_phone, '') || ' ' ||
+		coalesce(pager, '') || ' ' ||
+		coalesce(fax, '') || ' ' ||
+		coalesce(aim_screen_name, '') || ' ' ||
+		coalesce(msn_screen_name, '') || ' ' ||
+		coalesce(icq_number, '') || ' ' ||
+
+		coalesce(ha_line1, '') || ' ' ||
+		coalesce(ha_line2, '') || ' ' ||
+		coalesce(ha_city, '') || ' ' ||
+		coalesce(ha_state, '') || ' ' ||
+		coalesce(ha_postal_code, '') || ' ' ||
+
+		coalesce(wa_line1, '') || ' ' ||
+		coalesce(wa_line2, '') || ' ' ||
+		coalesce(wa_city, '') || ' ' ||
+		coalesce(wa_state, '') || ' ' ||
+		coalesce(wa_postal_code, '') || ' ' ||
+
+		coalesce(note, '') || ' ' ||
+		coalesce(current_information, '') || ' ' ||
+
+		coalesce(ha_cc.country_name, '') || ' ' ||
+		coalesce(wa_cc.country_name, '') || ' ' ||
+
+		coalesce(im_cost_center_name_from_id(department_id), '') || ' ' ||
+		coalesce(job_title, '') || ' ' ||
+		coalesce(job_description, '') || ' ' ||
+		coalesce(skills, '') || ' ' ||
+		coalesce(educational_history, '') || ' ' ||
+		coalesce(last_degree_completed, '') || ' ' ||
+		coalesce(termination_reason, '')
+
+	into	v_string
+	from
+		parties pa,
+		persons pe
+		LEFT OUTER JOIN users u ON (pe.person_id = u.user_id)
+		LEFT OUTER JOIN users_contact uc ON (pe.person_id = uc.user_id)
+		LEFT OUTER JOIN im_employees e ON (pe.person_id = e.employee_id)
+		LEFT OUTER JOIN country_codes ha_cc ON (uc.ha_country_code = ha_cc.iso)
+		LEFT OUTER JOIN country_codes wa_cc ON (uc.wa_country_code = wa_cc.iso)
+	where
+		pe.person_id	= new.person_id
+		and pe.person_id = pa.party_id
+	;
+
+	perform im_search_update(new.person_id, 'user', new.person_id, v_string);
+	return new;
+end;$$ language 'plpgsql';
+
+
+
+-- Frank Bergmann: 050709
+-- Dont add a trigger to "users": Users is being updated frequently when users 
+-- access the system, leading to serious slowdown of the machine.
+CREATE TRIGGER persons_tsearch_tr 
+AFTER INSERT or UPDATE ON persons
+FOR EACH ROW EXECUTE PROCEDURE persons_tsearch();
+
+
+
+
+
+-- update persons set first_names=first_names;
+create or replace function inline_0 ()
+returns integer as $body$
+declare
+	v_count		integer;
+	v_ctr		integer;
+	row		RECORD;
+begin
+	select count(*) into v_count from persons;
+	v_ctr := 0;
+	FOR row IN
+		select person_id from persons order by person_id
+	LOOP
+		RAISE NOTICE 'TSearch2: Updating person % of %: person_id=%', v_ctr, v_count, row.person_id;
+		update persons set first_names = first_names where person_id = row.person_id;
+		v_ctr := v_ctr + 1;
+	END LOOP;
+
+	return 0;
+end;$body$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
+---------------------------------------------------------
+-- Procedures
+
+
 create or replace function im_supervises_p (integer, integer)
 returns char as '
 DECLARE
