3358b0fed24496363b7e478c0675915c1dae1c41
Project Open <info@project-open.com>
2025-03-05 14:08:25 +0000

    v52: Cleanup upgrade scripts

--- sql/postgresql/upgrade/upgrade-5.0.3.0.0-5.0.3.0.1.sql
+++
@@ -1,8 +0,0 @@
--- upgrade-5.0.3.0.0-5.0.3.0.1.sql
-
-SELECT acs_log__debug('/packages/intranet-hr/sql/postgresql/upgrade/upgrade-5.0.3.0.0-5.0.3.0.1.sql','');
-
-
-update im_component_plugins
-set component_tcl = 'im_employee_info_component $user_id_from_search $return_url [im_opt_val -limit_to nohtml employee_view_name]'
-where plugin_name = 'User Employee Component';
